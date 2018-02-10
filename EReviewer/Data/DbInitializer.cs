02583fbabd9839a159787c8776d306d1d91b701b
rubyrose30 <rubyrose30.roque@gmail.com>
2018-02-10 20:20:56 +0800

    Initial Commit

---
+++ EReviewer/Data/DbInitializer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EReviewer.Data
+{
+    public class DbInitializer
+    {
+        public void Initialize(ApplicationDbContext context)
+        {
+            //TODO: Add initial or default values here
+        }
+    }
+}
